b62c5ab00c3d4cba8145f0878685c7e4e0307a40
Alexander Ding <alexander.jf.ding@gmail.com>
2022-03-17 23:53:42 -0400

    Removed pushd

--- compile.sh
+++ compile.sh
@@ -7,10 +7,9 @@
 # install Python3.8
 wget https://www.python.org/ftp/python/3.8.12/Python-3.8.12.tgz
 tar xzf Python-3.8.12.tgz
-pushd Python-3.8.12
-./configure --enable-optimizations
-make altinstall
-popd
+
+(cd Python-3.8.12; ./configure --enable-optimizations -prefix=$HOME)
+(cd Python-3.8.12; make altinstall)
 
 pip3.8 install -r requirements
 
